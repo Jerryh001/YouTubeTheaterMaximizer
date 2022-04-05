be21dae6b8aea4ff9a8026bc45adb36ece93fae2
Jerryh001 <Jerryh001@users.noreply.github.com>
2022-04-05 15:07:06 +0800

    0.1.0
    Create YouTubeTheaterMaximizer.user.js

---
+++ README.md
@@ -0,0 +1,11 @@
+# YouTubeTheaterMaximizer
+
+A user script to maximize the video window in Youtube Theater mode.
+
+Inspired by [MaximizerForYouTube](https://github.com/rotemdan/MaximizerForYouTube)
+
+## install
+
+1. Install [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) ([Greasemonkey](https://addons.mozilla.org/firefox/addon/greasemonkey/) should works too, not tested)
+
+2. Open [raw file of YouTubeTheaterMaximizer.user.js](https://github.com/Jerryh001/YouTubeTheaterMaximizer/raw/main/YouTubeTheaterMaximizer.user.js) and install it.
---
+++ YouTubeTheaterMaximizer.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name        YouTube Theater Maximizer
+// @description Maximizes the YouTube player to fill the entire browser viewport when in theater mode.
+// @license     MIT
+// @author      Jerryh001
+// @icon        http://www.google.com/s2/favicons?domain=youtube.com
+// @match       https://www.youtube.com/*
+// @version     0.1
+// @run-at      document-start
+// @grant       GM_addStyle
+// @namespace   https://github.com/Jerryh001
+// @homepageURL https://github.com/Jerryh001/YouTubeTheaterMaximizer
+// @require     https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.slim.min.js
+// ==/UserScript==
+
+let installed = false;
+
+// Install or uninstall full-size player page stylesheet if needed
+function startScript() {
+    const isWatchPage = inWatchPage();
+    if(!isWatchPage && installed) {
+        uninstall();
+    } else if (isWatchPage && !installed) {
+        install();
+        updateMastheadContainer();
+    }
+}
+
+function install() {
+    GM_addStyle(`#player-theater-container { max-height: 100vh !important; }`);
+    $(document).on("scroll", updateMastheadContainer);
+    $(document).on("fullscreenchange", updateMastheadContainer);
+    installed = true;
+}
+
+function uninstall() {
+    $("ytd-app").css("--ytd-masthead-height", "")
+    $(document).off("scroll", updateMastheadContainer);
+    $(document).off("fullscreenchange", updateMastheadContainer);
+    installed = false;
+}
+
+function updateMastheadContainer() {
+    const ytdApp = $("ytd-app");
+    ytdApp.css("--ytd-masthead-height", 0);
+    if ($(document).scrollTop() == 0) {
+        ytdApp.attr("masthead-hidden", true);
+    } else {
+        ytdApp.attr("masthead-hidden", null);
+    }
+}
+
+function inWatchPage() {
+	return location.href.startsWith("https://www.youtube.com/watch?");
+}
+
+$(() => {
+    if (window.self === window.top) {
+        startScript();
+    }
+    $(window).on("yt-page-data-updated", startScript);
+})
